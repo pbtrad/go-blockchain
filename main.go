da29f1701d4f009073f045a8e67015782644e6c1
paul <paulbrowne2@gmail.com>
2022-04-02 22:53:29 +0100

    Routes with http methods added with gorrila mux

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"crypto/md5"
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 type Block struct {
@@ -23,11 +30,11 @@ type BookCheckout struct {
 }
 
 type Book struct {
-	ID              string `json:"id"`
-	Title           string `json:"title"`
-	Author          string `json:"author"`
-	PublishableDate string `json:"publish_date"`
-	ISBN            string `json:"isbn"`
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Author      string `json:"author"`
+	PublishDate string `json:"publish_date"`
+	ISBN        string `json:"isbn"`
 }
 
 func (b *Block) generateHash() {
@@ -96,4 +103,85 @@ func (b *Block) validateHash(hash string) bool {
 		return false
 	}
 	return true
+}
+
+func getBlockchain(w http.ResponseWriter, r *http.Request) {
+	jbytes, err := json.MarshalIndent(BlockChain.blocks, "", " ")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+
+	io.WriteString(w, string(jbytes))
+}
+
+func writeBlock(w http.ResponseWriter, r *http.Request) {
+	var checkoutItem BookCheckout
+	if err := json.NewDecoder(r.Body).Decode(&checkoutItem); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("could not write Block: %v", err)
+		w.Write([]byte("could not write block"))
+		return
+	}
+
+	BlockChain.AddBlock(checkoutItem)
+	resp, err := json.MarshalIndent(checkoutItem, "", " ")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("could not marshal payload: %v", err)
+		w.Write([]byte("could not write block"))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
+func newBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	if err := json.NewDecoder(r.Body).Decode(&book); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("could not create: %v", err)
+		w.Write([]byte("could not create new Book"))
+		return
+	}
+
+	h := md5.New()
+	io.WriteString(h, book.ISBN+book.PublishDate)
+	book.ID = fmt.Sprintf("%x", h.Sum(nil))
+
+	resp, err := json.MarshalIndent(book, "", " ")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("could not marshal payload: %v", err)
+		w.Write([]byte("could not save book data"))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
+func main() {
+
+	BlockChain = NewBlockchain()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", getBlockchain).Methods("GET")
+	r.HandleFunc("/", writeBlock).Methods("POST")
+	r.HandleFunc("/new", newBook).Methods("POST")
+
+	go func() {
+
+		for _, block := range BlockChain.blocks {
+			fmt.Printf("Prev. hash: %x\n", block.PrevHash)
+			bytes, _ := json.MarshalIndent(block.Data, "", " ")
+			fmt.Printf("Data: %v\n", string(bytes))
+			fmt.Printf("Hash: %x\n", block.Hash)
+			fmt.Println()
+		}
+
+	}()
+	log.Println("Listening on port 3000")
+
+	log.Fatal(http.ListenAndServe(":3000", r))
 }
